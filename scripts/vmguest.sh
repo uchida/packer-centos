ddf5c448b84185e5a90bcca0c04ef954c58c9d11
Akihiro Uchida <uchida@turbare.net>
2015-06-20 11:16:26 +0900

    use wildcard to match virtualbox-iso and vmware-vmx

--- scripts/vmguest.sh
+++ scripts/vmguest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -ex
 case "$PACKER_BUILDER_TYPE" in
-  virtualbox-iso)
+  virtualbox-*)
     mount -o loop VBoxGuestAdditions.iso /mnt
     yum install -y gcc make gcc-c++ perl bzip2 "kernel-devel-$(uname -r)"
     sh /mnt/VBoxLinuxAdditions.run || :
     umount /mnt
     rm -rf VBoxGuestAdditions.iso
     ;;
-  vmware-iso)
+  vmware-*)
     yum install -y open-vm-tools
     ;;
 esac
